1eb839f163b0746bdf3165b9c86b2dfbf05699c4
connorshea15 <connorshea15@gmail.com>
2020-08-06 16:45:26 -0700

    My express server is returning index.html and notes.html for '/' and '/notes' respectively

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
-
-
-
+const path = require('path');
+const { notes } = require('./Develop/db/db.json');
+const PORT = 3004;
 const app = express();
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
