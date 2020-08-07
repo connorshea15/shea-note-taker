e1044a7d29c41b30fc5f65e1db90eb385d2af037
connorshea15 <connorshea15@gmail.com>
2020-08-07 13:08:16 -0700

    changing the port to accomodate heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const notes = require('./Develop/db/db.json');
-const PORT = 3004;
+const PORT = process.env.PORT || 3004;
 const app = express();
 
 app.use(express.static('Develop/public'));
