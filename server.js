609ad8000cdb08467c7ae3bfaf5ea62bf1f5ab89
connorshea15 <connorshea15@gmail.com>
2020-08-06 17:00:09 -0700

    GET notes works returning json notes according to insomnia core

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
-const { notes } = require('./Develop/db/db.json');
+const notes = require('./Develop/db/db');
 const PORT = 3004;
 const app = express();
+
+// parse incoming string or array data
+// app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+// app.use(express.json());
+
+console.log(notes);
+
+// api call to get notes in json format
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
 
 // serves index.html page
 app.get('/', (req, res) => {
